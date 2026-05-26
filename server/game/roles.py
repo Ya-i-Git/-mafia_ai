c4a7452f66334b37c12e0069a2856ce7320fd1e0
Scatana <ivan.efimov25@mail.ru>
2026-05-26 15:35:12 +0300

    change structure

--- server/game/roles.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class Role(str, Enum):
-    MAFIA = "mafia"
-    DON = "don"
-    SHERIFF = "sheriff"
-    DOCTOR = "doctor"
-    CIVILIAN = "civilian"
--- server/services/auth_service.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from typing import Dict
-
-users_db: Dict[str, str] = {}  # username -> token
-
-def register_or_login(username: str) -> str:
-    if username not in users_db:
-        users_db[username] = str(uuid.uuid4())
-    return users_db[username]
